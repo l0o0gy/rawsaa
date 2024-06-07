1bba127d553b16e527c80d8e5a143d5305030db6
l0o0gy <minamahone9620@gmail.com>
2024-06-07 23:05:42 +0300

    add navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './img/logo.png'
+function Navbar() {
+  return (
+    <div className='flex justify-between pl-6 shadow-lg'>
+        <div className='flex '>
+            <img className='w-8 h-9 mt-3'
+                 src={logo} alt='logo'/>
+           <h1 className='pt-5 font-bold text-lg mr-80 '>awssa</h1>
+        </div>
+        <input className=' w-3/4 seacrch border-2 pl-10 p-1 mr-80 mt-5 mb-2 rounded-lg '
+        type='text' placeholder='search...'/>
+    </div>
+  )
+}
+
+export default Navbar

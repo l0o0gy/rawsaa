3adf39f0941c0a625d730867dd43d75bfa0a7161
l0o0gy <minamahone9620@gmail.com>
2024-06-10 18:07:00 +0300

    add save page

---
+++ src/Components/Savepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Drawer from './Drawer'
+
+function Savepage() {
+  return (
+    <div className=' bg-red-200 text-center h-screen'>
+    <Navbar/>
+    <Drawer/>
+      Savepage
+    </div>
+  )
+}
+
+export default Savepage

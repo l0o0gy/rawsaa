0fcbf67a8a30e11599d85bf50430ba5526372ec3
l0o0gy <minamahone9620@gmail.com>
2024-06-13 22:31:38 +0300

    add card to home page

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Drawer from './Drawer'
+import Card from './Card'
+import {useData} from "./contacts/store"
+
 function Homepage() {
+  const items = useData();
   return (
     <div className='bg-white text-center h-screen'>
       <Navbar/>
       <Drawer/>
-      Homepage</div>
+      <div className="  grid grid-cols-4 ml-64 mt-10 ">
+      {items.map(item => (
+        <Card key={item.id} item={item} />
+      ))}
+    </div></div>
   )
 }
 
-export default Homepage
+export default Homepage;

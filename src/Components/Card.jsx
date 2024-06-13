a8ce594c469fdb7c30130a1b034e3bc5781853d2
l0o0gy <minamahone9620@gmail.com>
2024-06-13 22:30:40 +0300

    add card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
-function Card() {
+import React from "react"
+function Card({item}) {
   return (
-    <div>
-      card
+    <div className=" card  flex mb-10 ">
+      <div className=" border-2 rounded-md flex mt-2 ">
+      <div>
+      <img src={item.img} alt={item.title} className="w-60  rounded-md pb-3"/>
+      <h3 className="text-lg font-semibold shadow-md">{item.title}</h3>
+      </div>
+      </div>
     </div>
   )
 }
 
-export default Card
+export default Card;
 

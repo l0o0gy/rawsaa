fc4294d771ea20927d05004c28e44e73e5a8c590
l0o0gy <minamahone9620@gmail.com>
2024-06-14 02:37:39 +0300

    add path for login & sign up page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Coverpage from './Components/Coverpage';
 import Homepage from './Components/Homepage';
 import Savepage from './Components/Savepage';
 import { DataProvider } from './Components/contacts/store';
+import Login from './Components/Login';
+import Singup from './Components/Signup';
 
 function App() {
   return (
@@ -13,8 +15,10 @@ function App() {
       <DataProvider>
         <Routes>
           <Route path='/' element={<Coverpage/>}/>
-            <Route path='/homepage' element={<Homepage/>}/>
+          <Route path='/homepage' element={<Homepage/>}/>
           <Route path='/savepage' element={<Savepage/>}/>
+          <Route path='/loginpage' element={<Login/>}/>
+          <Route path='/signuppage' element={<Singup/>}/>
         </Routes>
         </DataProvider>
       </BrowserRouter>

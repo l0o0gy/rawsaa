ae5451c2b7b10ff7f8024919cd45dc4bf98920f3
l0o0gy <minamahone9620@gmail.com>
2024-06-13 22:28:13 +0300

    add Provider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Coverpage from './Components/Coverpage';
 import Homepage from './Components/Homepage';
 import Savepage from './Components/Savepage';
+import { DataProvider } from './Components/contacts/store';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <DataProvider>
         <Routes>
           <Route path='/' element={<Coverpage/>}/>
-          <Route path='/homepage' element={<Homepage/>}/>
+            <Route path='/homepage' element={<Homepage/>}/>
           <Route path='/savepage' element={<Savepage/>}/>
-
         </Routes>
+        </DataProvider>
       </BrowserRouter>
-      
     </div>
   );
 }

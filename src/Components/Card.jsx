54a1873e4de722495c63cd1dfbffd0d58331be05
l0o0gy <minamahone9620@gmail.com>
2024-06-14 16:00:26 +0300

    .

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ function Card({item}) {
     <div className=" card  flex mb-10 ">
       <div className=" border-2 rounded-md flex mt-2 ">
       <div>
-      <img src={item.img} alt={item.title} className="w-60  rounded-md pb-3"/>
+      <img src={item.img} alt={item.title} className="w-60   rounded-md pb-3"/>
       <h3 className="text-lg font-semibold shadow-md">{item.title}</h3>
       </div>
       </div>

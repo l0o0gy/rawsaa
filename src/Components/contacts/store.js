3684b67f076d3ceef38a5785193db5872d124b48
l0o0gy <minamahone9620@gmail.com>
2024-06-19 20:56:42 +0300

    change photo

--- src/Components/contacts/store.js
+++ src/Components/contacts/store.js
@@ -1,18 +1,26 @@
 import React from "react";
-import img from '../../assets/img/img.jpg'
+import img1 from '../../assets/img/houseware.jpg'
+import img2 from '../../assets/img/Officeware.jpg'
+import img3 from '../../assets/img/Electronics.jpg'
+import img4 from '../../assets/img/Furniture.jpg'
+import img5 from '../../assets/img/Car Accessories.png'
+import img6 from '../../assets/img/books.jpg'
+import img7 from '../../assets/img/Antiques.jpg'
+import img8 from '../../assets/img/Electrical devices.jpg'
+
 import { createContext, useContext, useState} from 'react';
 
 const DataContext = createContext();
 
 const data = [
-    { id: 1, img: img, title: "Houseware" },
-    { id: 2, img: img, title: "Office Ware" },
-    { id: 3, img: img, title: "Electronics" },
-    { id: 4, img: img, title: "Furniture" },
-    { id: 5, img: img, title: "Car Accessories" },
-    { id: 6, img: img, title: "Books" },
-    { id: 7, img: img, title: "Antiques" },
-    { id: 8, img: img, title: "Electrical Devices" }
+    { id: 1, img: img1, title: "Houseware" },
+    { id: 2, img: img2, title: "Office Ware" },
+    { id: 3, img: img3, title: "Electronics" },
+    { id: 4, img: img4, title: "Furniture" },
+    { id: 5, img: img5, title: "Car Accessories" },
+    { id: 6, img: img6, title: "Books" },
+    { id: 7, img: img7, title: "Antiques" },
+    { id: 8, img: img8, title: "Electrical Devices" }
 ];
 
 export const DataProvider = ({ children }) => {

90cf76da09d12cf03b916166674f3d4544212037
l0o0gy <minamahone9620@gmail.com>
2024-06-21 01:46:39 +0300

    add changes

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const PostCard = ({ post }) => {
   return (
-    <div className=' w-40 border p-4 m-2'>
-      {post.photo && <img src={URL.createObjectURL(post.photo)} alt={post.itemName} className='w-40 ' />}
-      <h3>Name:{post.itemName}</h3>
-      <p>Description: {post.description}</p>
-      <p> location {post.location}</p>
+    <div className=' w-40 border  p-4 m-2 rounded-md'>
+      {post.photo && <img src={URL.createObjectURL(post.photo)} alt={post.itemName} className='w-40 rounded-md drop-shadow mb-2 ' />}
+      <h3>{post.itemName}</h3>
+      <p> {post.description}</p>
+      <p> {post.location}</p>
       <button className="bg-orange-500 hover:bg-orange-600 p-2 rounded-md mt-2 text-white drop-shadow-md w-full" >Read more </button>
       {/* <div>
         {post.categories.map((category, index) => (

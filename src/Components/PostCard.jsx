e5e9e9beb34490ee6c6854ccb08bb6a1355ebba4
l0o0gy <minamahone9620@gmail.com>
2024-08-01 19:34:46 +0300

    change text of alart delete post button

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -9,57 +9,86 @@ import Typography from '@mui/material/Typography';
 import { orange } from '@mui/material/colors';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Added useNavigate
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
+import IconButton from '@mui/material/IconButton';
 
 const PostCard = ({ post }) => {
-  const [showPost, setShowPost] = useState(true);
+  const [isBookmarked, setIsBookmarked] = useState(() => {
+    const savedPosts = JSON.parse(localStorage.getItem('savedPosts')) || [];
+    return savedPosts.some(savedPost => savedPost.id === post.id);
+  });
 
   if (!post) {
-    return null; // or a placeholder like a loading spinner
+    return null; 
   }
+
+  const handleBookmarkClick = () => {
+    let savedPosts = JSON.parse(localStorage.getItem('savedPosts')) || [];
+    
+    if (!isBookmarked) {
+      savedPosts = [...savedPosts, post];
+      localStorage.setItem('savedPosts', JSON.stringify(savedPosts));
+      setIsBookmarked(true);
+    } else {
+      savedPosts = savedPosts.filter(savedPost => savedPost.id !== post.id);
+      localStorage.setItem('savedPosts', JSON.stringify(savedPosts));
+      setIsBookmarked(false);
+    }
+  };
 
   return (
     <div>
-      {showPost && (
-        <Link to={`/posts/${post.id}`} state={{ post }} style={{ textDecoration: 'none' }}>
-          <Card sx={{ height: 500 }}>
-            <CardHeader
-              sx={{ marginLeft: '-10px', marginTop: '-10px', fontSize: '10px' }}
-              avatar={
-                <img
-                  style={{ maxWidth: 50, objectFit: 'cover', borderRadius: 100 }}
-                  src={`https://mena.alraed1.com/imgUsers/2dcccde4-67f3-486c-8d42-55d4cda172d4.jpg`}
-                />
-              }
-              title={post.user_name}
-              subheader={post.date}
-            />
-            <CardMedia
-              sx={{ marginTop: '-13px' }}
-              component="img"
-              height="90"
-              image={`https://mena.alraed1.com/imgPosts/${post.img_id}.jpg`}
-              alt={post.item_name}
-            />
-            <CardContent sx={{ marginTop: '-13px' }}>
-              <Typography variant="body2" color="text.secondary">
-                {post.item_name}
-                {post.description}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing sx={{ marginTop: '-30px' }}>
+      {post && (
+        <Card sx={{ height: '100%' }}>
+          <CardHeader
+            sx={{
+              marginLeft: '-10px',
+              marginTop: '-10px',
+              fontSize: '10px',
+              display: 'grid',
+              gridTemplateColumns: 'repeat(3, 1fr)',
+            }}
+            avatar={
+              <img
+                style={{ maxWidth: 50, objectFit: 'cover', borderRadius: 100 }}
+                src={`https://mena.alraed1.com/imgUsers/2dcccde4-67f3-486c-8d42-55d4cda172d4.jpg`}
+                alt="User Avatar"
+              />
+            }
+            title={post.user_name}
+            subheader={post.date}
+            action={
+              <IconButton onClick={handleBookmarkClick}>
+                {isBookmarked ? <BookmarkAddedIcon /> : <BookmarkAddIcon />}
+              </IconButton>
+            }
+          />
+          <CardMedia
+            sx={{ marginTop: '-13px' }}
+            component="img"
+            height="90"
+            image={`https://mena.alraed1.com/imgPosts/${post.img_id}.jpg`}
+            alt={post.item_name}
+          />
+          <CardContent sx={{ marginTop: '-13px' }}>
+            <Typography variant="body2" color="text.secondary">
+              {post.item_name}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing sx={{ marginTop: '-30px' }}>
+            <Link to={`/posts/${post.id}`} state={{ post }} style={{ textDecoration: 'none' }}>
               <Box sx={{ marginTop: '3px' }}>
-                <div>
-                  <Button
-                    sx={{ bgcolor: orange[500], color: 'white', width: 140, height: 50 }}
-                  >
-                    Read More
-                  </Button>
-                </div>
+                <Button
+                  sx={{ bgcolor: orange[500], color: 'white', width: 'full'}}
+                >
+                  Read More
+                </Button>
               </Box>
-            </CardActions>
-          </Card>
-        </Link>
+            </Link>
+          </CardActions>
+        </Card>
       )}
     </div>
   );

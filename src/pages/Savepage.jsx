ad6fe0bb7aeb278283796351b12dced84e5576e8
l0o0gy <minamahone9620@gmail.com>
2024-08-01 19:35:51 +0300

    add useeffect using localStorge to save the post

--- src/pages/Savepage.jsx
+++ src/pages/Savepage.jsx
@@ -3,31 +3,41 @@ import { useNavigate } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Drawer from '../Components/Drawer';
 import Cookies from 'js-cookie';
+import PostCard from '../Components/PostCard';
 
 function Savepage() {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [savedPosts, setSavedPosts] = useState([]);
 
   useEffect(() => {
-    // Logic to determine if the user is authenticated
     const authStatus = Cookies.get('isAuthenticated') === 'true';
     setIsAuthenticated(authStatus);
 
     if (!authStatus) {
-      navigate('/Loginpage'); // Redirect to login page if not authenticated
+      navigate('/Loginpage'); 
+    } else {
+      const savedPosts = JSON.parse(localStorage.getItem('savedPosts')) || [];
+      setSavedPosts(savedPosts);
     }
   }, [navigate]);
 
   if (!isAuthenticated) {
-    return null; // Render nothing or a loader while redirecting
+    return null;
   }
 
   return (
     <div className='text-center h-screen'>
-      <Navbar />
+      {/* <Navbar /> */}
       <Drawer isAuthenticated={isAuthenticated} />
-      <div className="text-center bg-red-500 sm:ml-64 sm:mt-10">
-        <h1>Savepage</h1>
+      <div className="text-center bg-red-100 sm:ml-64 sm:mt-10 p-4 flex-row">
+        <div style={{display: 'grid', gridTemplateColumns: 'repeat(3,1fr)', gap: 2, width:'100%',height:'100%' }}>
+          {savedPosts.length > 0 ? (
+            savedPosts.map(post => <PostCard key={post.id} post={post} />)
+          ) : (
+            <p>No saved posts found.</p>
+          )}
+        </div>
       </div>
     </div>
   );

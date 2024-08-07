64d363fc3d72eb62ec09c039ec1e59f7ebef7647
l0o0gy <minamahone9620@gmail.com>
2024-08-08 01:27:29 +0300

    change css style of cards

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -5,20 +5,35 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { orange } from '@mui/material/colors';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import { Link, useNavigate } from 'react-router-dom'; // Added useNavigate
+import { red, orange } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import { Link } from 'react-router-dom';
 
-import PropTypes from 'prop-types';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Skeleton from '@mui/material/Skeleton';
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const StyledCardMedia = styled(CardMedia)({
+  height: 400,
+  width: '100%',
+  objectFit: 'cover'
+});
 
 const PostCard = ({ post }) => {
   const [isBookmarked, setIsBookmarked] = useState(() => {
@@ -26,8 +41,10 @@ const PostCard = ({ post }) => {
     return savedPosts.some(savedPost => savedPost.id === post.id);
   });
 
+  const [expanded, setExpanded] = useState(false);
+
   if (!post) {
-    return null; 
+    return null;
   }
 
   const handleBookmarkClick = () => {
@@ -43,61 +60,43 @@ const PostCard = ({ post }) => {
       setIsBookmarked(false);
     }
   };
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div>
-      {post && (
-        <Card sx={{ height: '100%' }}>            
-          <Stack direction="row" spacing={0} 
-            sx={{
-              // marginLeft: '10px',
-              marginTop: '10px',
-              fontSize: '10px',
-               display: 'flex',
-                justifyContent:'space-between',
-                height:70,
-               }}
-                         >
-            <Avatar
-              src="https://mena.alraed1.com/imgUsers/2dcccde4-67f3-486c-8d42-55d4cda172d4.jpg"
-              alt="User Avatar"
-              sx={{ marginLeft: '10px'}}
-            />
-            <CardHeader
-              sx={{
-                fontSize: '10px',
-                marginTop:'-10px',
-                // display: 'flex',
-                // justifyContent:'space-evenly'
-               }}
-
-              title={post.user_name}
-              subheader={post.date}
-
-           />  
-            <IconButton onClick={handleBookmarkClick}>
-              {isBookmarked ? <BookmarkAddedIcon /> : <BookmarkAddIcon />}
-            </IconButton>
-          </Stack>
-          <CardMedia
-            sx={{ marginTop: '-13px' }}
-            component="img"
-            height="90"
-            image={`https://mena.alraed1.com/imgPosts/${post.img_id}.jpg`}
-            alt={post.item_name}
-          />
-          <CardContent sx={{ marginTop: '-13px' }}>
-            <Typography variant="body2" color="text.secondary">
-              {post.item_name}
-            </Typography>
-          </CardContent>
-            <Link to={`/posts/${post.id}`} state={{ post }} style={{ textDecoration: 'none' }}>
-              <button  className='mb-2 p-2 bg-orange-500  text-white rounded w-11/12'>
-               Read More
-              </button>
-            </Link>
-        </Card>
-      )}
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar src="https://mena.alraed1.com/imgUsers/2dcccde4-67f3-486c-8d42-55d4cda172d4.jpg" alt="User Avatar" />
+        }
+        action={
+          <IconButton onClick={handleBookmarkClick} aria-label="bookmark">
+          {isBookmarked ? <BookmarkAddedIcon /> : <BookmarkAddIcon />}
+          </IconButton>
+        }
+        title={post.user_name}
+        subheader={post.date}
+      />
+      <StyledCardMedia
+        component="img"
+        image={`https://mena.alraed1.com/imgPosts/${post.img_id}.jpg`}
+        alt={post.item_name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {post.item_name}
+        </Typography>
+      </CardContent>
+        <CardContent>
+          <Link to={`/posts/${post.id}`} state={{ post }} style={{ textDecoration: 'none' }}>
+            <button className='mb-2 p-2 bg-orange-500 text-white rounded w-full'>
+              Read More
+            </button>
+          </Link>
+        </CardContent>
+    </Card>
   );
 };
 

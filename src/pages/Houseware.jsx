19a6a4332c08b464a78d96c4e1d602a4bd03c61c
l0o0gy <minamahone9620@gmail.com>
2024-08-08 21:12:29 +0300

    add seach input

--- src/pages/Houseware.jsx
+++ src/pages/Houseware.jsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
-import Drawer from '../Components/Drawer';
 import PostCard from '../Components/PostCard';
 import Addpost from '../Components/Addpost';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 import ResultList from '../Components/ResultList'
+import Divider from '@mui/material/Divider';
+import ResponsiveDrawer from '../Components/Drawer.jsx';
+import { useData } from "../Components/contacts/store";
 
 function Houseware() {
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   const [results,setResults] = useState([])
   const cookies = Cookies.get('token');
+  const [searchTerm, setSearchTerm] = useState(''); 
+  const items = useData();
+
+  const handleSearch = (term) => {
+    setSearchTerm(term); 
+      axios
+        .get(`https://mena.alraed1.com/postSearch/${searchTerm}`)
+        .then((response) => {
+          setPosts(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+
+  };
 
   
   useEffect(() => {
@@ -61,20 +78,31 @@ const handlePostAdded=()=>{
 
   return (
     <div className=' text-center h-screen'>
-      <Navbar />
-      <Drawer setResults={setResults} />
-      <ResultList results={results}/>
+      {/* <Navbar /> */}
+      <ResponsiveDrawer handleSearch={handleSearch} />
+      {/* <Drawer setResults={setResults} /> */}
+      {/* <ResultList results={results}/> */}
       <Addpost  onPostAdded={handlePostAdded} />
-
-      <div className="text-center sm:ml-64 sm:mt-10">
-        <h1 >Houseware</h1>
-        <div className='grid grid-cols-2 ml-2  mt-3 sm:ml-0 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
-          {posts.map((post, index ) => (
+      {searchTerm === ''? (
+        <>
+        <div className="text-center sm:ml-60 ">
+          <div className='grid grid-cols-2 ml-1  mt-3 sm:ml-0 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 sm:gap-4 gap-3  p-1 sm:mb-5'>
+            {posts.map((post, index ) => (
+              <PostCard key={index} post={post} />
+            ))} 
+          </div>
+        </div>
+        </>
+      ):(
+        <div className="text-center sm:ml-60 ">
+        <div className='grid grid-cols-2 ml-1  mt-3 sm:ml-0 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 sm:gap-4 gap-3  p-1 sm:mb-5'>
+          {items.map((post, index ) => (
             <PostCard key={index} post={post} />
           ))} 
         </div>
-        {/* <AddPost setPosts={setPosts} /> */}
       </div>
+      )}
+
     </div>
   );
 }

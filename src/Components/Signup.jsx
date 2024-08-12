06297ccd98e27e5c60e313a6ae576abce719e49e
l0o0gy <minamahone9620@gmail.com>
2024-08-12 16:38:23 +0300

    change css and add first and last name

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -18,17 +18,17 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Alert from '@mui/material/Alert';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AddAPhotoRoundedIcon from '@mui/icons-material/AddAPhotoRounded';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import img from '../assets/img/rawsshalogo.jpg'
 
 const theme = createTheme();
-
 export default function SignUpFinal() {
   const [data, setData] = useState({ name: '', email: '', password: '', phone_number: '' });
   const [photo, setPhoto] = useState(null);
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
@@ -57,47 +57,56 @@ export default function SignUpFinal() {
     });
 
     const formData = new FormData();
-    formData.append('user', renamedFile);
+    formData.append("user", renamedFile);
 
     try {
-      const { data } = await axios.post('https://mena.alraed1.com/imgUsers', formData);
+      const { data } = await axios.post(
+        "https://mena.alraed1.com/imgUsers",
+        formData
+      );
       return data.filePath;
     } catch (error) {
-      console.error('Error uploading image:', error);
+      console.error("Error uploading image:", error);
       return null;
     }
   };
 
   const handleSubmit = async (e) => {
+    let id = 0;
     e.preventDefault();
-    const id = uuidv4();
+    if (photo) {
+      id = uuidv4();
+    }
     await uploadImg(id);
 
-    axios.post('https://mena.alraed1.com/register', {
-      username: data.name,
-      email: data.email,
-      password: data.password,
-      user_number: data.phone_number,
-      status: 'active',
-      role: 'user',
-      img_id: id,
-    })
+    axios
+      .post("https://mena.alraed1.com/register", {
+        username: data.name,
+        email: data.email,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        password: data.password,
+        user_number: data.phone_number,
+        status: "active",
+        role: "user",
+        img_id: id,
+      })
       .then((res) => {
-        setSuccessMessage('Sign-up successful! Redirecting to login page...');
-        setErrorMessage('');
+        setSuccessMessage("Sign-up successful! Redirecting to login page...");
+        setErrorMessage("");
         setTimeout(() => {
-          navigate('/loginpage');
+          navigate("/loginpage");
         }, 3000);
       })
       .catch((err) => {
-        setErrorMessage('Sign-up failed. Please try again.');
-        setSuccessMessage('');
+        setErrorMessage("Sign-up failed. Please try again.");
+        setSuccessMessage("");
       });
   };
 
   return (
-    <ThemeProvider theme={theme} sx={{ overflow: 'hidden' }}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+    <ThemeProvider theme={theme} sx={{ overflow: "hidden" }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -105,44 +114,69 @@ export default function SignUpFinal() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              `url(${img})`,
+            backgroundImage: `url(${img})`,
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'left',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "left",
           }}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} >
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6}>
           <Box
             sx={{
               my: 2,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ bgcolor: 'secondary.main', backgroundColor: 'orange' }}>
+            <Avatar
+              sx={{ bgcolor: "secondary.main", backgroundColor: "orange" }}
+            >
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign Up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <FormControl fullWidth margin="normal">
-                <FormLabel>Photo</FormLabel>
-                <input type="file" accept="image/*" onChange={handlePhotoChange} />
+                <input
+                  type="file"
+                  hidden
+                  id="imgUser"
+                  accept="image/*"
+                  onChange={handlePhotoChange}
+                />
               </FormControl>
               <TextField
                 margin="normal"
                 required
                 fullWidth
                 id="name"
-                label="Name"
-                name="name"
-                autoComplete="name"
-                value={data.name}
+                label="first_name"
+                name="first_name"
+                autoComplete="first_name"
+                value={data.first_name}
+                onChange={handleChange}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="last_name"
+                name="last_name"
+                autoComplete="last_name"
+                value={data.last_name}
                 onChange={handleChange}
                 autoFocus
               />
@@ -158,7 +192,7 @@ export default function SignUpFinal() {
                 value={data.email}
                 onChange={handleChange}
               />
-              <FormControl fullWidth >
+              <FormControl fullWidth>
                 <TextField
                   margin="normal"
                   required
@@ -166,7 +200,7 @@ export default function SignUpFinal() {
                   id="email"
                   label="password"
                   name="password"
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   value={data.password}
                   onChange={handleChange}
                   InputProps={{
@@ -188,20 +222,40 @@ export default function SignUpFinal() {
                 value={data.phone_number}
                 onChange={handleChange}
               />
+                             <FormControl>
+
+              <label
+                for="imgUser"
+                 className="  bg-gray-500 p-2 rounded px-4 m-0 "
+              >
+
+                <AddAPhotoRoundedIcon/>
+                <span class=' pl-1 pt-4 mt-4'>Add Photo</span>
+              </label>
+              </FormControl>
+
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: '#f98306' }}
+                sx={{ mt: 3, mb: 2, backgroundColor: "#f98306" }}
               >
                 Sign Up
               </Button>
-              {successMessage && <Alert severity="success" sx={{ mt: 2 }}>{successMessage}</Alert>}
-              {errorMessage && <Alert severity="error" sx={{ mt: 2 }}>{errorMessage}</Alert>}
+              {successMessage && (
+                <Alert severity="success" sx={{ mt: 2 }}>
+                  {successMessage}
+                </Alert>
+              )}
+              {errorMessage && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  {errorMessage}
+                </Alert>
+              )}
               <Typography
                 endDecorator={<Link href="/loginpage">Log in</Link>}
                 fontSize="sm"
-                sx={{ alignSelf: 'center' }}
+                sx={{ alignSelf: "center" }}
               >
                 Already have an account?
               </Typography>

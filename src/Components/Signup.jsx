f63e95bd906473738716b0b8431f17997821b9f5
l0o0gy <minamahone9620@gmail.com>
2024-08-19 22:31:33 +0300

    change logo

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -21,7 +21,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddAPhotoRoundedIcon from '@mui/icons-material/AddAPhotoRounded';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
-import img from '../assets/img/rawsshalogo.jpg'
+import img from '../assets/img/rawsshalogo.png'
 
 const theme = createTheme();
 export default function SignUpFinal() {

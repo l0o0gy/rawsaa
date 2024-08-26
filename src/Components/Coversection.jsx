22e359656803317bd257f178525e01370776ec6c
l0o0gy <minamahone9620@gmail.com>
2024-08-26 18:53:48 +0300

    change the font

--- src/Components/Coversection.jsx
+++ src/Components/Coversection.jsx
@@ -15,11 +15,11 @@ function Coversection({handleSearch}) {
   return (
     <div className=' mb-3 sm:mb-0 sm:ml-0 bg-gray-100'>
       <div className=' grid grid-cols-2'>
-        <div className='  w-80 text-start pl-4 sm:ml-60 sm:w-3/4 '>
-          <h1 className='mt-20 text-xl font-bold ml-0 sm:text-4xl sm:ml-5  ' >
+        <div className='  w-80 text-start pl-4 sm:ml-60  sm:w-96 lg:w-3/4 '>
+          <h1 className='mt-20 text-xl sm:text-2xl lg:text-4xl font-bold ml-0 sm:ml-5 ' >
             Rawssha with others and </h1>
-            <h1 className=' text-xl font-bold  sm:text-4xl sm:ml-5 sm:mt-2 text-orange-500 '>get what you need.</h1>
-          <p className='  mr-40 text-xs mt-2 sm:text-sm sm:mt-5 sm:ml-5 sm:w-96 '>
+            <h1 className=' text-xl font-bold  sm:text-2xl lg:text-4xl sm:ml-5 sm:mt-1 lg:mt-2 text-orange-500 '>get what you need.</h1>
+          <p className='  mr-40 text-xs mt-2 sm:text-sm sm:mt-5 sm:ml-5 sm:w-80 lg:w-96 '>
             The first Web App  in iraq to changing your old stuff that you don't 
             need anymore with what you need it, 
             faster than you think and without any cost 
@@ -27,7 +27,7 @@ function Coversection({handleSearch}) {
             <div className='hidden sm:flex'>
 
             <input              
-            className=' w-60 sm:w-80 sm:ml-5 border h-10 mt-5  rounded-md p-2 ' 
+            className=' w-60 sm:w-80 sm:ml-5 border h-10 mt-5 sm:mb-2 rounded-md p-2 ' 
             type='text' 
             placeholder='search...'
             onInput={(e) => handleSearch(e.target.value)}
@@ -41,7 +41,7 @@ function Coversection({handleSearch}) {
           
         </div>
         <div>
-          <img src={img} alt=" coverimg" className=' mt-20 mr-0 ml-0 sm:w-3/4 sm:mt-0 sm:ml-20 sm:pl-20 sm:pt-10'/>
+          <img src={img} alt=" coverimg" className=' mt-20 mr-0 ml-0 sm:w-3/4 sm:mt-0 sm:ml-24 lg:ml-20 sm:pl-20 sm:pt-10'/>
         </div>
       </div>
     </div>
